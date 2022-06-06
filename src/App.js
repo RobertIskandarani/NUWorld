1d372b72328d7bc0f7d0eb76fd83474ba88dd888
Roberto Ezequiel Iskandarani <64543556+Robert-Iskandarani@users.noreply.github.com>
2022-06-06 13:20:12 -0300

    se agrega loading page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/header/Header.jsx';
 import Body from './components/Body.jsx';
 import Footer from './components/Footer.jsx';
 
 const App = () => {
-    return (
-        <div className="container" id="app">
-            <Header />
-            <Body />
-            <Footer />
-        </div>
-    );
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 10000);
+    }, []);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div className="container" id="app">
+                <Header />
+                <Body />
+                <Footer />
+            </div>
+        );
+    }
 };
 
 export default App;
--- src/components/Roadmap/hooks.jsx
+++ src/components/Roadmap/hooks.jsx
@@ -1,39 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-const defaultState = { image: undefined, status: "loading" };
+const defaultState = { image: undefined, status: 'loading' };
 export const useImage = (url, crossOrigin) => {
-  const res = useState(defaultState);
-  const image = res[0].image;
-  const status = res[0].status;
+    const res = useState(defaultState);
+    const image = res[0].image;
+    const status = res[0].status;
 
-  const setState = res[1];
+    const setState = res[1];
 
-  useEffect(() => {
-    if (!url) return;
-    const img = document.createElement("img");
+    useEffect(() => {
+        if (!url) return;
+        const img = document.createElement('img');
 
-    function onload() {
-      setState({ image: img, status: "loaded" });
-    }
+        function onload() {
+            setState({ image: img, status: 'loaded' });
+        }
 
-    function onerror() {
-      setState({ image: undefined, status: "failed" });
-    }
+        function onerror() {
+            setState({ image: undefined, status: 'failed' });
+        }
 
-    img.addEventListener("load", onload);
-    img.addEventListener("error", onerror);
-    crossOrigin && (img.crossOrigin = crossOrigin);
-    img.src = url;
+        img.addEventListener('load', onload);
+        img.addEventListener('error', onerror);
+        crossOrigin && (img.crossOrigin = crossOrigin);
+        img.src = url;
 
-    return () => {
-      img.removeEventListener("load", onload);
-      img.removeEventListener("error", onerror);
-      setState(defaultState);
-    };
-  }, [url, crossOrigin, setState]);
+        return () => {
+            img.removeEventListener('load', onload);
+            img.removeEventListener('error', onerror);
+            setState(defaultState);
+        };
+    }, [url, crossOrigin, setState]);
 
-  // return array because it it better to use in case of several useImage hooks
-  // const [background, backgroundStatus] = useImage(url1);
-  // const [patter] = useImage(url2);
-  return [image, status];
+    return [image, status];
 };
