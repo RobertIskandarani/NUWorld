5d8f0d3f0762862b0a5e04f236dca0891cf4a497
Roberto Ezequiel Iskandarani <64543556+Robert-Iskandarani@users.noreply.github.com>
2022-06-08 15:05:38 -0300

    se agrega loading page

--- src/index.js
+++ src/index.js
@@ -3,9 +3,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const loadingPage = document.querySelector('.loading-page');
+const loader = document.querySelector('.loader');
+
+const changeProgress = progress => {
+    loader.style.width = `${progress}%`;
+};
+
+setTimeout(() => changeProgress(22), 1000);
+setTimeout(() => changeProgress(45), 2000);
+setTimeout(() => changeProgress(85), 4600);
+setTimeout(() => changeProgress(99.9), 5266);
+setTimeout(() => changeProgress(100), 8000);
+
+const showLoader = () => loadingPage.classList.remove('loader--hide');
+
+const hideLoader = () => loadingPage.classList.add('loader--hide');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+setTimeout(
+    () =>
+        root.render(
+            <React.StrictMode>
+                <App hideLoader={hideLoader} showLoader={showLoader} />
+            </React.StrictMode>
+        ),
+    7000
 );
+
+root.render();
