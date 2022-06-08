ae480a148fe67ef923dbcc38b612b535746aea90
Roberto Ezequiel Iskandarani <64543556+Robert-Iskandarani@users.noreply.github.com>
2022-06-08 16:10:04 -0300

    se agrega import de font al principio

--- src/components/body/PlayerDesktop.jsx
+++ src/components/body/PlayerDesktop.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Iframe from 'react-iframe';
 const PlayerDesktop = () => {
+    const src = 'https://player.vimeo.com/video/716537930?autoplay=1&muted=1&controls=0&loop=1';
     return (
         <Iframe
-            src="https://player.vimeo.com/video/716537930?autoplay=1&muted=1&controls=0&loop=1"
+            src={src}
             width="100%"
             height="100%"
             frameBorder="0"
--- src/components/body/PlayerMobile.jsx
+++ src/components/body/PlayerMobile.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Iframe from 'react-iframe';
 
 const PlayerMobile = () => {
+    const src = 'https://player.vimeo.com/video/718044664?autoplay=1&muted=1&controls=0&loop=1';
     return (
         <Iframe
-            src="https://player.vimeo.com/video/718044664?autoplay=1&muted=1&controls=0&loop=1"
+            src={src}
             width="100%"
             height="100%"
             frameBorder="0"
