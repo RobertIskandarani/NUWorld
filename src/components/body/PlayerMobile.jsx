79e6999605a388c49888b2fb0bbd68e8546e6fc4
Roberto Ezequiel Iskandarani <64543556+Robert-Iskandarani@users.noreply.github.com>
2022-06-14 10:42:50 -0300

    video desde vimeo

--- src/components/body/PlayerMobile.jsx
+++ src/components/body/PlayerMobile.jsx
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player';
 const PlayerMobile = () => {
     return (
         <ReactPlayer
-            url={require('./../../videos/Banner_superior_Web_Okey_Mobile.mp4')}
+            url="https://vimeo.com/718044664"
             width="100%"
             height="100%"
             loop
