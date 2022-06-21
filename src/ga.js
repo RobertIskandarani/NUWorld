60fdaadacf1b4f2ff80d263e41156d42043066a5
Roberto Ezequiel Iskandarani <64543556+Robert-Iskandarani@users.noreply.github.com>
2022-06-21 15:43:12 -0300

    se modifica console

--- src/ga.js
+++ src/ga.js
@@ -2,12 +2,12 @@ import ReactGA from 'react-ga4';
 
 const TRACKING_ID = 'G-T4EEP82VG5';
 ReactGA.initialize(TRACKING_ID);
-console.log('me conecte con ', TRACKING_ID);
 
 const useAnalyticsEventTracker = (category = 'Blog category') => {
     const eventTracker = (action = 'test action', label = 'test label') => {
         ReactGA.event({ category, action, label });
     };
+    console.log('me conecte con ', TRACKING_ID, 'con el ', eventTracker);
     return eventTracker;
 };
 
