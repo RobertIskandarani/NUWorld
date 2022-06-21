7fd77bbe5d0fc7c459c72f08ed3a7fb8c248faa8
Roberto Ezequiel Iskandarani <64543556+Robert-Iskandarani@users.noreply.github.com>
2022-06-21 15:41:36 -0300

    se agrega console.log

--- src/ga.js
+++ src/ga.js
@@ -2,6 +2,7 @@ import ReactGA from 'react-ga4';
 
 const TRACKING_ID = 'G-T4EEP82VG5';
 ReactGA.initialize(TRACKING_ID);
+console.log('me conecte con ', TRACKING_ID);
 
 const useAnalyticsEventTracker = (category = 'Blog category') => {
     const eventTracker = (action = 'test action', label = 'test label') => {
