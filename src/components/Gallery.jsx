d947bd7ac07cb2135b36c58f5e1c51041cbcc101
Roberto Ezequiel Iskandarani <64543556+Robert-Iskandarani@users.noreply.github.com>
2022-07-26 16:09:33 -0300

    gallery casera

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import nft from '../data/nft';
 import Carousel from 'react-elastic-carousel';
+import Slider from 'react-slick';
+
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 import './../styles/Gallery.css';
 
 const Gallery = () => {
     const [width, setWindowWidth] = useState(0);
+    const [index, setIndex] = useState(0);
+    const [hiddenBack, setHiddenBack] = useState(true);
+    const [hiddenForward, setHiddenForward] = useState(false);
 
     useEffect(() => {
         updateDimensions();
@@ -17,20 +23,83 @@ const Gallery = () => {
         const width = window.innerWidth;
         setWindowWidth(width);
     };
+
     return (
         <div className="gallery">
             <h1 className="gallery-title">Gallery</h1>
             <p className="meet-the-population">Coming soon...</p>
-            <Carousel
-                enableSwipe={true}
-                enableMouseSwipe={true}
-                itemsToShow={width <= 768 ? 1 : 3}
-                itemsToScroll={width <= 768 ? 1 : 3}
-            >
-                {nft.map(i => (
-                    <img className="image-carrousel" src={i.image} alt={i.id} key={i.id} />
-                ))}
-            </Carousel>
+            <div>
+                <div className="carousel">
+                    <IoIosArrowBack
+                        size={30}
+                        className={hiddenBack ? 'arrow-back-hidden' : 'arrow-back'}
+                        onClick={() => {
+                            if (index === 1) {
+                                setHiddenBack(true);
+                            }
+                            setIndex(index - 1);
+                            setHiddenForward(false);
+                        }}
+                    />
+                    <div className="images-carousel">
+                        {nft
+                            .filter(i => i.index === index)
+                            .map(i => (
+                                <img
+                                    className={'image-carrousel'}
+                                    src={i.image}
+                                    alt={i.id}
+                                    key={i.id}
+                                />
+                            ))}
+                    </div>
+                    <IoIosArrowForward
+                        size={30}
+                        className={hiddenForward ? 'arrow-forward-hidden' : 'arrow-forward'}
+                        onClick={() => {
+                            if (index === 2) {
+                                setHiddenForward(true);
+                            }
+                            setIndex(index + 1);
+                            setHiddenBack(false);
+                        }}
+                    />
+                </div>
+                <div className="dots">
+                    <button
+                        className={index === 0 ? 'dot-selected' : 'dot'}
+                        onClick={() => {
+                            setIndex(0);
+                            setHiddenBack(true);
+                            setHiddenForward(false);
+                        }}
+                    ></button>
+                    <button
+                        className={index === 1 ? 'dot-selected' : 'dot'}
+                        onClick={() => {
+                            setIndex(1);
+                            setHiddenBack(false);
+                            setHiddenForward(false);
+                        }}
+                    ></button>
+                    <button
+                        className={index === 2 ? 'dot-selected' : 'dot'}
+                        onClick={() => {
+                            setIndex(2);
+                            setHiddenBack(false);
+                            setHiddenForward(false);
+                        }}
+                    ></button>
+                    <button
+                        className={index === 3 ? 'dot-selected' : 'dot'}
+                        onClick={() => {
+                            setIndex(3);
+                            setHiddenBack(false);
+                            setHiddenForward(true);
+                        }}
+                    ></button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/data/nft.js
+++ src/data/nft.js
@@ -2,50 +2,62 @@ const nft = [
     {
         id: 0,
         image: require('../assets/NFT para Carrousel/Lock 1.png'),
+        index: 0,
     },
     {
         id: 1,
         image: require('../assets/NFT para Carrousel/Lock 2.png'),
+        index: 0,
     },
     {
         id: 2,
         image: require('../assets/NFT para Carrousel/Lock 3.png'),
+        index: 0,
     },
     {
         id: 3,
         image: require('../assets/NFT para Carrousel/Lock 4.png'),
+        index: 1,
     },
     {
         id: 4,
         image: require('../assets/NFT para Carrousel/Lock 5.png'),
+        index: 1,
     },
     {
         id: 5,
         image: require('../assets/NFT para Carrousel/Lock 6.png'),
+        index: 1,
     },
     {
         id: 6,
         image: require('../assets/NFT para Carrousel/Lock 7.png'),
+        index: 2,
     },
     {
         id: 7,
         image: require('../assets/NFT para Carrousel/Lock 8.png'),
+        index: 2,
     },
     {
         id: 8,
         image: require('../assets/NFT para Carrousel/Lock 9.png'),
+        index: 2,
     },
     {
         id: 9,
         image: require('../assets/NFT para Carrousel/Lock 10.png'),
+        index: 3,
     },
     {
         id: 10,
         image: require('../assets/NFT para Carrousel/Lock 11.png'),
+        index: 3,
     },
     {
         id: 11,
         image: require('../assets/NFT para Carrousel/Lock 12.png'),
+        index: 3,
     },
 ];
 
