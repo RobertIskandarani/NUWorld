4a1da4822b50c1e05c1080854722ffe16b4fed7b
Roberto Ezequiel Iskandarani <64543556+Robert-Iskandarani@users.noreply.github.com>
2022-08-19 12:19:02 -0300

    se modifican las fotos de la galeria

--- src/data/nft.js
+++ src/data/nft.js
@@ -1,51 +1,51 @@
 const nft = [
     {
         id: 0,
-        image: require('../assets/NFT para Carrousel/Lock 1.png'),
+        image: require('../assets/NFT para Carrousel/1.png'),
     },
     {
         id: 1,
-        image: require('../assets/NFT para Carrousel/Lock 2.png'),
+        image: require('../assets/NFT para Carrousel/2.png'),
     },
     {
         id: 2,
-        image: require('../assets/NFT para Carrousel/Lock 3.png'),
+        image: require('../assets/NFT para Carrousel/3.png'),
     },
     {
         id: 3,
-        image: require('../assets/NFT para Carrousel/Lock 4.png'),
+        image: require('../assets/NFT para Carrousel/4.png'),
     },
     {
         id: 4,
-        image: require('../assets/NFT para Carrousel/Lock 5.png'),
+        image: require('../assets/NFT para Carrousel/5.png'),
     },
     {
         id: 5,
-        image: require('../assets/NFT para Carrousel/Lock 6.png'),
+        image: require('../assets/NFT para Carrousel/6.png'),
     },
     {
         id: 6,
-        image: require('../assets/NFT para Carrousel/Lock 7.png'),
+        image: require('../assets/NFT para Carrousel/7.png'),
     },
     {
         id: 7,
-        image: require('../assets/NFT para Carrousel/Lock 8.png'),
+        image: require('../assets/NFT para Carrousel/8.png'),
     },
     {
         id: 8,
-        image: require('../assets/NFT para Carrousel/Lock 9.png'),
+        image: require('../assets/NFT para Carrousel/9.png'),
     },
     {
         id: 9,
-        image: require('../assets/NFT para Carrousel/Lock 10.png'),
+        image: require('../assets/NFT para Carrousel/10.png'),
     },
     {
         id: 10,
-        image: require('../assets/NFT para Carrousel/Lock 11.png'),
+        image: require('../assets/NFT para Carrousel/11.png'),
     },
     {
         id: 11,
-        image: require('../assets/NFT para Carrousel/Lock 12.png'),
+        image: require('../assets/NFT para Carrousel/12.png'),
     },
 ];
 
